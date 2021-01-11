439fc0de1801ea44adae0e83573df68623c4c420
AhmedRagab <aragab22@gmail.com>
2021-01-11 22:58:46 +0200

    initialize project

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import express from "express";
+require("dotenv").config();
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
+const app: express.Application = express();
+const port = process.env.PORT;
+
+// body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+// test endpoints
+app.get("/test", (req: express.Request, res: express.Response) => {
+  res.json("test api endpoint");
+});
+app.post("/test", (req: express.Request, res: express.Response) => {
+  const { name, age } = req.body;
+  res.json({
+    name,
+    age,
+  });
+});
+// to connect to mongodb url
+mongoose.connect(
+  process.env.MONGOURI ||
+    "your local database url like {'mongodb://localhost:27017/your database name'}",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  },
+  () => {
+    console.log("connected to database");
+  }
+);
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
