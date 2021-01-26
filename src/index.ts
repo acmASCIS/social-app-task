c503e78445b7eac399d9c940a2f5df2a8456caea
Abanoub321 <abanoubmilad12@gmail.com>
2021-01-26 14:46:43 +0200

    User

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,26 @@
 import express from "express";
 require("dotenv").config();
 import mongoose from "mongoose";
-import bodyParser from "body-parser";
-
+import userRoute from './routes/UserRoute';
 const app: express.Application = express();
 const port = process.env.PORT;
 
 // body parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(express.json());
+app.use(express.urlencoded());
+
+
+
+//endpoints
+
+app.use('/users',userRoute);
+
 
 // test endpoints
 app.get("/test", (req: express.Request, res: express.Response) => {
   res.json("test api endpoint");
 });
+
 app.post("/test", (req: express.Request, res: express.Response) => {
   const { name, age } = req.body;
   res.json({
@@ -21,10 +28,12 @@ app.post("/test", (req: express.Request, res: express.Response) => {
     age,
   });
 });
+
+
 // to connect to mongodb url
 mongoose.connect(
   process.env.MONGOURI ||
-    "your local database url like {'mongodb://localhost:27017/your database name'}",
+    'mongodb://localhost:27017/something',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
